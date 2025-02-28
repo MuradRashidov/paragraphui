cdd70d3c5c6e0dab5ba07d9c60d792b933fc1ea2
MuradRashidov <muradrashidov995@gmail.com>
2025-02-28 21:13:26 +0400

    posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Posts from "@/components/Posts";
 import { fetchPosts } from "@/lib/actions/postAction";
 import { DEFAULT_PAGE_SIZE } from "@/lib/constants";
 import { getSession } from "@/lib/session";
+import { Post } from "@/lib/types/modelTypes";
 import Image from "next/image";
 
 type Props = {
@@ -10,15 +11,25 @@ type Props = {
 }
 export default async function Home({ searchParams }: Props) {
   const { page } = await searchParams;
-  const { posts, totalPosts } = await fetchPosts({ page: page ? +page : undefined });
+  let postss :Post[]= [];
+  let totalPostss = 0;
+  try {
+    const { posts, totalPosts } = await fetchPosts({ page: page ? +page : undefined });
+    postss = posts;
+    totalPostss = totalPosts;
+
+  } catch (error) {
+    console.log(error);
+    
+  }
   //const session = await getSession();
   //console.log(session);
 
   return (
     <div className="bg-gradient-to-br text-white">
       <Hero />
-      <Posts posts={posts || []} currentPage={page ? +page : 1}
-        totalPages={Math.ceil(totalPosts / DEFAULT_PAGE_SIZE)} />
+      <Posts posts={postss || []} currentPage={page ? +page : 1}
+        totalPages={Math.ceil(totalPostss / DEFAULT_PAGE_SIZE)} />
     </div>
   );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 //export const API_URL = "http://localhost:8000/";
+//export const API_URL = "https://67c1ebbe28c48f00995ec666--paragraphapi.netlify.app/";
 export const API_URL = "https://paragraphapi.onrender.com/";
 export const DEFAULT_PAGE_SIZE = 3;
 export const HERO_TEXT = "Düşüncəli müzakirələr və maraqlı məzmunların bir araya gəldiyi canlı bir icmanın üzvü olun."
